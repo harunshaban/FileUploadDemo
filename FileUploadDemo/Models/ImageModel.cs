57a715aa960ecaa42dfe6579fe9c7ba324c69b76
Harun Shaban <harun.sabban@gmail.com>
2021-10-20 11:48:11 +0200

    Created Image Model

---
+++ FileUploadDemo/Models/ImageModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUploadDemo.Models
+{
+    public class ImageModel
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        [Column(TypeName = "navchar(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "navchar(120)")]
+        public string ImageName { get; set; }
+    }
+}
